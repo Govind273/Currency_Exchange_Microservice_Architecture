8d97525ce144d1fff7207ad80642e07c4cdc51de
Govind <shremish@indiana.edu>
2018-02-14 14:56:28 -0500

    added welocome service

--- limits-service/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,13 @@
 package com.in28minutes.microservices.limitsservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LimitsConfigurationController {
 
+	@GetMapping("welcome")
+	public String sayWelcome() {
+		return "Hello You";
+	}
 }
